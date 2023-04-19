d4aa92b2338b8435021b80ab9a7f73b10b9066f1
yasminmccann <c19717111@mytudublin.ie>
2023-04-19 12:06:38 +0100

    Commit 2

--- app/src/main/java/com/example/fyp/AgilityTestActivity.java
+++ app/src/main/java/com/example/fyp/AgilityTestActivity.java
@@ -2,6 +2,7 @@ package com.example.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -24,6 +25,8 @@ public class AgilityTestActivity extends AppCompatActivity {
     private ViewGroup mainLayout;
     private ImageView image1;
     private ImageView image2;
+
+    private float xCoOrdinate, yCoOrdinate;
 
     //Position
     private float ballDownY;
@@ -65,100 +68,129 @@ public class AgilityTestActivity extends AppCompatActivity {
 
         }.start();*/
 
-            mainLayout = (RelativeLayout) findViewById(R.id.main);
-            image1 = (ImageView) findViewById(R.id.circle1);
-            image2 = (ImageView) findViewById(R.id.circle2);
+        mainLayout = (RelativeLayout) findViewById(R.id.main);
+        image1 = (ImageView) findViewById(R.id.circle1);
+        image2 = (ImageView) findViewById(R.id.circle2);
 
-            this.score = (TextView) findViewById(R.id.score);
+        this.score = (TextView) findViewById(R.id.score);
 
-            //image1.setOnTouchListener(onTouchListener());
-            // image2.setOnTouchListener(onTouchListener());
+        //image1.setOnTouchListener(onTouchListener());
+        // image2.setOnTouchListener(onTouchListener());
 
-            //retrieving screen size
-            WindowManager wm = getWindowManager();
-            Display disp = wm.getDefaultDisplay();
-            Point size = new Point();
-            disp.getSize(size);
-            screenWidth = size.x;
-            screenHeight = size.y;
+        //retrieving screen size
+        WindowManager wm = getWindowManager();
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
 
-            Thread t = new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        while (!isInterrupted()) {
-                            Thread.sleep(100);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Render();
-                                }
-                            });
-                        }
-                    } catch (InterruptedException e) {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(100);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Render();
+                            }
+                        });
                     }
+                } catch (InterruptedException e) {
                 }
-            };
-
-            t.start();
-
-        }
-
-        public void Render ()
-        {
-            changePos();
-            if (Collision(image1, image2)) {
-                points++; //You dont need findView Textview score for that exists in OnCreate Method
-                this.score.setText("Score:" + points);
             }
-        }
-/*
-        public boolean CheckCollision(ImageView image1, ImageView image2){
-            Rect r1 = new Rect(image1.getLeft(), image1.getTop(), image1.getRight(), image1.getBottom());
-            Rect r2 = new Rect(image2.getLeft(), image2.getTop(), image2.getRight(), image2.getBottom());
-            return r1.intersect(r2);
-        }*/
-        private boolean Collision (ImageView image1, ImageView image2){
-            Rect BallRect = new Rect();
-            image2.getHitRect(BallRect);
-            Rect NetRect = new Rect();
-            image1.getHitRect(NetRect);
-            return BallRect.intersect(NetRect);
-        }
+        };
 
-        public void changePos ()
-        {
+        t.start();
 
-            //down
-            ballDownY += 10;
+        image1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        xCoOrdinate = view.getX() - event.getRawX();
+                        yCoOrdinate = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        view.animate().x(event.getRawX() + xCoOrdinate).y(event.getRawY() + yCoOrdinate).setDuration(0).start();
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
+
+        image2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        xCoOrdinate = view.getX() - event.getRawX();
+                        yCoOrdinate = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        view.animate().x(event.getRawX() + xCoOrdinate).y(event.getRawY() + yCoOrdinate).setDuration(0).start();
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    public void Render() {
+        if (Collision(image1, image2)) {
+            points++; //You dont need findView Textview score for that exists in OnCreate Method
+            this.score.setText("Score:" + points);
+        }
+    }
+
+    private boolean Collision(ImageView image1, ImageView image2) {
+        Rect r1 = new Rect();
+        image2.getHitRect(r1);
+        Rect r2 = new Rect();
+        image1.getHitRect(r2);
+        return r1.intersect(r2);
+
+
+    }
+
+   // public void changePos() {
+
+        //down
+            /*ballDownY += 10;
             if (image2.getY() > screenHeight) {
                 ballDownX = (float) Math.floor((Math.random() * (screenWidth - image2.getWidth())));
                 ballDownY = -100.0f;
 
-            }
-            image2.setY(ballDownY);
-            image2.setX(ballDownX);
+            }*/
+       // image2.setY(ballDownY);
+       // image2.setX(ballDownX);
 
-            //make net follow finger
-            mainLayout.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View view, MotionEvent event) {
-                    x = event.getX();
-                    y = event.getY();
 
-                    if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                        image1.setX(x);
-                        image1.setY(y);
-                    }
-                    return true;
+        //make net follow finger
+        /*mainLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                x = event.getX();
+                y = event.getY();
+
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    image1.setX(x);
+                    image1.setY(y);
                 }
+                return true;
+            }
 
-            });
+        });*/
 
-        }
-    }
-
-   /* private View.OnTouchListener onTouchListener() {
+   // }
+    /* private View.OnTouchListener onTouchListener() {
         return new View.OnTouchListener() {
 
             @SuppressLint("ClickableViewAccessibility")
@@ -191,5 +223,6 @@ public class AgilityTestActivity extends AppCompatActivity {
                 mainLayout.invalidate();
                 return true;
             }
-        };*/
-        //}
+        };
+    }*/
+}
